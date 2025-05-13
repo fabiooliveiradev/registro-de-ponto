63800ed66aa3a6073ef3159e1737710e3e24ee95
fabiooliveiradev <fabionetnews@gmail.com>
2025-05-12 21:45:06 -0300

    Criação da funções Create:  "Title, DataBox, Label e BigNumbers".

--- script.js
+++ script.js
@@ -3,9 +3,38 @@ const CreateLogoScreen = (imgLink, father) => {
     container.classList.add('container');
     const img = document.createElement('img');
     img.setAttribute('src', imgLink);
-    img.classList.add('img-logo-screen')
+    img.classList.add('img-logo-screen');
     container.appendChild(img);
     document.querySelector(father).appendChild(container);
+}
+
+const CreateTitle = (title, style, father) => {
+
+    const ttl = document.createElement('h1');
+    ttl.innerText = title;
+    ttl.classList.add(style);
+    document.querySelector(father).appendChild(ttl);
+}
+
+const CreateDataBox = (style, father) => {
+    const dataBox = document.createElement('div');
+    dataBox.classList.add(style);
+    document.querySelector(father).appendChild(dataBox);
+
+}
+
+const CreateLabel = (text, style, father) => {
+    const label = document.createElement('label');
+    label.classList.add(style);
+    label.innerText = text;
+    document.querySelector(father).appendChild(label);
+}
+
+const CreateBigNumbers = (value, style, father) => {
+    const bigNumber = document.createElement('div');
+    bigNumber.innerText = value;
+    bigNumber.classList.add(style);
+    document.querySelector(father).appendChild(bigNumber);
 }
 
 const CreateButton = (value, style, father) => {
@@ -15,9 +44,10 @@ const CreateButton = (value, style, father) => {
     document.querySelector(father).appendChild(btn);
 }
 
-const CreateBudgetScreen = () => {
-    
-}
-
-CreateButton('Testar', 'btn-1', '.container');
-// CreateLogoScreen('https://drive.google.com/thumbnail?id=1Kt9O8YkbBMOT9F-h1-QvEhHXZngS6j3a', '.container');
+CreateTitle('Registro de Ponto', 'title', '.container');
+CreateTitle('Dashboard', 'sub-title', '.container');
+CreateDataBox('data-box-grid', '.container');
+CreateLabel('Saldo mês', 'label', '.data-box-grid');
+CreateLabel('Saldo total', 'label', '.data-box-grid');
+CreateBigNumbers(value = '05:30h', style = 'positive', '.data-box-grid');
+CreateBigNumbers(value = '11:40h', style = 'negative', '.data-box-grid');
